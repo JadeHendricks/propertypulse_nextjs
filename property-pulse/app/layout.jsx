b18685abb97658f76fe034dfc8a6f4908ab22ecd
Jade <hendricksjade@gmail.com>
2024-03-25 16:45:46 +0200

    init(nextjs): Created layout, home page and added in styles.

---
+++ property-pulse/app/layout.jsx
@@ -0,0 +1,20 @@
+import '@/assets/styles/globals.css';
+
+//global metadata
+export const metadata = {
+    title: 'PropertyPulse | Find the perfect rental',
+    description: 'Find your dream rental property',
+    keywords: 'rental, find rentals, find properties'
+}
+
+const MainLayout = ({ children }) => {
+  return (
+    <html lang='en'>
+        <body>
+            <div>MainLayout</div> 
+        </body>
+    </html>
+  )
+}
+
+export default MainLayout;
